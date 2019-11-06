02e1512bdab9ea4efc1253aa3cc3deab7b3447bd
Ahmad Nafis Shoba <ahmad.shoba@loket.com>
2019-11-06 16:30:50 +0700

    Login screen dummy

--- app/src/main/java/com/erebor/tomkins/pos/view/login/LoginActivity.java
+++ app/src/main/java/com/erebor/tomkins/pos/view/login/LoginActivity.java
@@ -1,19 +1,28 @@
 package com.erebor.tomkins.pos.view.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.erebor.tomkins.pos.R;
 import com.erebor.tomkins.pos.base.BaseActivity;
-import com.erebor.tomkins.pos.databinding.ActivityMainBinding;
+import com.erebor.tomkins.pos.databinding.ActivityLoginBinding;
 import com.erebor.tomkins.pos.di.AppComponent;
+import com.erebor.tomkins.pos.view.dashboard.DashboardActivity;
 
-public class LoginActivity extends BaseActivity<ActivityMainBinding> {
+public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding.text.setText("Login");
+        binding.buttonLogin.setOnClickListener(v -> {
+            binding.setLoading(true);
+            binding.buttonLogin.postDelayed(() -> {
+                binding.setLoading(false);
+                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                finish();
+            }, 3000);
+        });
     }
 
     @Override
@@ -23,6 +32,6 @@ public class LoginActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     public int getLayout() {
-        return R.layout.activity_main;
+        return R.layout.activity_login;
     }
 }
