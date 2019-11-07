6ad9e461175e731052ab1211a00cb1100d879def
Ahmad Nafis Shoba <ahmad.shoba@loket.com>
2019-11-07 07:03:35 +0700

    Dashboard layout

--- app/src/main/java/com/erebor/tomkins/pos/view/dashboard/DashboardActivity.java
+++ app/src/main/java/com/erebor/tomkins/pos/view/dashboard/DashboardActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 
 import com.erebor.tomkins.pos.R;
 import com.erebor.tomkins.pos.base.BaseActivity;
+import com.erebor.tomkins.pos.data.ui.UserUiModel;
+import com.erebor.tomkins.pos.databinding.ActivityDashboardBinding;
 import com.erebor.tomkins.pos.databinding.ActivityMainBinding;
 import com.erebor.tomkins.pos.di.AppComponent;
 
-public class DashboardActivity extends BaseActivity<ActivityMainBinding> {
+public class DashboardActivity extends BaseActivity<ActivityDashboardBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding.text.setText("Dashboard");
+        UserUiModel userUiModel = new UserUiModel();
+        userUiModel.setName("Frodo Bagins");
+        userUiModel.setPosition("Hobbits");
+        binding.setUser(userUiModel);
     }
 
     @Override
@@ -23,6 +28,6 @@ public class DashboardActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     public int getLayout() {
-        return R.layout.activity_main;
+        return R.layout.activity_dashboard;
     }
 }
