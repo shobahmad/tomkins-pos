efa1a78a33abb566e2cb38a115053f1acc1bd512
Ahmad Nafis Shoba <ahmad.shoba@loket.com>
2020-02-19 17:35:46 +0700

    Scanner options : add google vision

--- app/src/main/java/com/erebor/tomkins/pos/view/sale/SaleActivity.java
+++ app/src/main/java/com/erebor/tomkins/pos/view/sale/SaleActivity.java
@@ -17,19 +17,26 @@ import com.erebor.tomkins.pos.data.ui.SaleHistoryUiModel;
 import com.erebor.tomkins.pos.data.ui.SaleUiModel;
 import com.erebor.tomkins.pos.databinding.ActivitySaleBinding;
 import com.erebor.tomkins.pos.di.AppComponent;
+import com.erebor.tomkins.pos.tools.SharedPrefs;
 import com.erebor.tomkins.pos.view.callback.ItemQtyHandler;
+import com.erebor.tomkins.pos.view.dashboard.DashboardActivity;
+import com.erebor.tomkins.pos.view.scan.VisionScannerActivity;
 import com.erebor.tomkins.pos.view.scan.ZynxScannerActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 public class SaleActivity extends BaseActivity<ActivitySaleBinding> {
 
     SaleHistoryAdapter saleHistoryAdapter;
+    @Inject
+    SharedPrefs sharedPrefs;
 
     @Override
     public void inject(AppComponent appComponent) {
-
+        appComponent.doInjection(this);
     }
 
     @Override
@@ -68,7 +75,21 @@ public class SaleActivity extends BaseActivity<ActivitySaleBinding> {
             binding.setConfirmed(true);
             fetchHistory();
         });
-        binding.buttonScan.setOnClickListener(v -> startActivityForResult(new Intent(SaleActivity.this, ZynxScannerActivity.class), 1));
+        binding.buttonScan.setOnClickListener(v -> {
+
+            String scanner = sharedPrefs.getString(getResources().getString(R.string.setting_key_camera), "");
+            if (scanner.equals("")) {
+                startActivityForResult(new Intent(this, ZynxScannerActivity.class), 1);
+                return;
+            }
+
+            if (scanner.equals("zxing")) {
+                startActivityForResult(new Intent(this, ZynxScannerActivity.class), 1);
+                return;
+            }
+
+            startActivityForResult(new Intent(this, VisionScannerActivity.class), 1);
+        });
     }
 
     private void fetchProductInfo(String productId) {
