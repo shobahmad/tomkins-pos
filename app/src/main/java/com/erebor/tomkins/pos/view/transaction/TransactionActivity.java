abed5cf82d9fd78c8894e313a9d52d6e66eaa655
Ahmad Nafis Shoba <ahmad.shoba@loket.com>
2020-03-28 18:24:29 +0700

    Remove tricky scanner

--- app/src/main/java/com/erebor/tomkins/pos/view/transaction/TransactionActivity.java
+++ app/src/main/java/com/erebor/tomkins/pos/view/transaction/TransactionActivity.java
@@ -70,10 +70,10 @@ public class TransactionActivity extends BaseActivity<ActivityTransactionBinding
         syncUploadViewModel = ViewModelProviders.of(this, factory).get(SyncUploadViewModel.class);
 
         binding.buttonScan.setOnClickListener(v -> {
-            if (true) {
-                transactionViewModel.scanBarcode("89949060800701");
-                return;
-            }
+//            if (true) {
+//                transactionViewModel.scanBarcode("89949060800701");
+//                return;
+//            }
             String scanner = sharedPrefs.getString(getResources().getString(R.string.setting_key_camera), "");
             if (scanner.equals("")) {
                 startActivityForResult(new Intent(TransactionActivity.this, VisionScannerActivity.class), 1);
