35d3e7eeebb6bc626cbd3435fc2f64b867d10616
Ahmad Nafis Shoba <ahmad.shoba@loket.com>
2020-03-31 20:22:46 +0700

    Download info

--- app/src/main/java/com/erebor/tomkins/pos/data/local/model/DownloadLastUpdateModel.java
+++ app/src/main/java/com/erebor/tomkins/pos/data/local/model/DownloadLastUpdateModel.java
@@ -1,27 +1,36 @@
 package com.erebor.tomkins.pos.data.local.model;
 
+import androidx.annotation.Nullable;
 import androidx.room.DatabaseView;
 
 import java.util.Date;
 
 @DatabaseView("select article.last_update article, barcode.last_update barcode, brand.last_update brand, gender.last_update gender, size.last_update size, stock.last_update stock, event.last_update event, eventdetail.last_update eventdetail from " +
-              "(select last_update from MSART order by last_update desc limit 1) article, " +
-              "(select last_update from MSBARCODE order by last_update desc limit 1) barcode, " +
-              "(select last_update from MSBRAND order by last_update desc limit 1) brand, " +
-              "(select last_update from MSGENDER order by last_update desc limit 1) gender, " +
-              "(select last_update from MSUKURAN order by last_update desc limit 1) size, " +
-              "(select last_update from STOKREAL order by last_update desc limit 1) stock, " +
-              "(select last_update from EVENTHARGA order by last_update desc limit 1) event, " +
-              "(select last_update from EVENTHARGADET order by last_update desc limit 1) eventdetail "
+              "(select last_update, count(*) from MSART order by last_update desc limit 1) article, " +
+              "(select last_update, count(*) from MSBARCODE order by last_update desc limit 1) barcode, " +
+              "(select last_update, count(*) from MSBRAND order by last_update desc limit 1) brand, " +
+              "(select last_update, count(*) from MSGENDER order by last_update desc limit 1) gender, " +
+              "(select last_update, count(*) from MSUKURAN order by last_update desc limit 1) size, " +
+              "(select last_update, count(*) from STOKREAL order by last_update desc limit 1) stock, " +
+              "(select last_update, count(*) from EVENTHARGA order by last_update desc limit 1) event, " +
+              "(select last_update, count(*) from EVENTHARGADET order by last_update desc limit 1) eventdetail "
              )
 public class DownloadLastUpdateModel  {
+    @Nullable
     public Date article;
+    @Nullable
     public Date barcode;
+    @Nullable
     public Date brand;
+    @Nullable
     public Date gender;
+    @Nullable
     public Date size;
+    @Nullable
     public Date stock;
+    @Nullable
     public Date event;
+    @Nullable
     public Date eventdetail;
 
     public Date getArticle() {
